48d97104eebba596c05879db79acbdac3851a588
korivernon <44047473+korivernon@users.noreply.github.com>
2020-02-29 00:21:58 +0400

    working on library

--- testcode/csvtest.cpp
+++ testcode/csvtest.cpp
@@ -5,21 +5,26 @@
 #include<fstream>
 
 using namespace std;
+
+void loadLibrary(){
+    vector<string> files;
+    files.push_back("BookCaseA");
+    files.push_back("BookCaseB");
+    files.push_back("BookCaseC");
+    cout << files[0] << endl;
+}
 class Book{
     //Title, Author	ISBN	Year	Number_of_copies
     private:
         string title, author, isbn, year;
         int copies;
     public:
-        Book(string ti, string au, string isb, string yr, int c, string code = ''){
+        Book(string ti, string au, string isb, string yr, int c){
             title = ti;
             author = au;
             year = yr;
             isbn = isb;
-            copies = c
-        }
-        const string getTitle(){
-            return this->title;
+            copies = c;
         }
 };
 class BookShelf{
@@ -29,11 +34,13 @@ class BookShelf{
     public:
         void addBook(const Book & bookie){
             
-            bookshelf.pushback(bookie);
+            bookshelf.push_back(bookie);
             if (bookshelf.size() == 10) {
                 cout << "Bookcase is full\n";
                 //BookCase.incrementShelf();
             }
+        }
+        /*
         const Book & getBookCode(string call) const{
             return bookshelf[call]; //'a1.1' 1
         }
@@ -41,30 +48,35 @@ class BookShelf{
             for(int i = 0; i < 10; i++){ //iterating through the bookshelves
                 if (title == bookshelf[i].getTitle()) return i+1; // if you find it here then return it +1
             }
-        }
+        }*/
 };
+
 class BookCase {
-    int shelf=0;
-    bookcase <vector<BookShelf>;
+    static int shelf;
+    vector<Book> bs;
+    vector<BookShelf> bookcase;
     public:
         void addBookShelf(<Book> bs){
             if(shelf == 5) cout << "Error\n";
-            bookcase[shelf].pushback(bs); //add bookshelf to the bookcase at index shelf
-            shelf+=1; //increment shelf after
+            bookcase[shelf] = bs; //add bookshelf to the bookcase at index shelf
+            shelf++; //increment shelf after
+            bs = new BookShelf;
         }
+        /*
         const Book & getBook(string code)const {
             return bookcase[code[1]].getBookCode(code[3]); //take 'a1.1' for example.
         }
-        const Book & getBookByTitle(string title)const{
+        const Book getBookByTitle(string title){
             for (int i=0; i<5; i++) {
                  if (bookcase[i].findTitle(title)){
                      //assign code once object is created
                  }
             }
-        }
+        }*/
 
-}
+};
+
 int main(){
-    Book book;
+    cout<< "hi" << endl;
     return 0;
 }
